ee85f2edcf704c138865f27950bb0e22b1ab14e2
Narayan Sau <narayansau@yahoo.com>
2019-12-21 13:14:16 +0530

    initial commit

---
+++ src/main/java/com/example/dpenendencyinjection/DpenendencyinjectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.dpenendencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpenendencyinjectionApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run( DpenendencyinjectionApplication.class, args );
+	}
+
+}
---
+++ src/test/java/com/example/dpenendencyinjection/DpenendencyinjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dpenendencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpenendencyinjectionApplicationTests{
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
